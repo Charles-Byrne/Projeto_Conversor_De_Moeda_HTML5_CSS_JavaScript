98d60519dd9dbf7c702a52ab68fed49bdf7aaf6d
Charles Byrne <125279820+Charles-Byrne@users.noreply.github.com>
2023-03-21 22:58:04 -0300

    pro
    Project

---
+++ script.js
@@ -0,0 +1,50 @@
+const button = document.getElementById('convert-button')
+const select = document.getElementById('currency-select')
+const dolar = 5.24
+const euro = 5.7
+
+const convertValues = () => {
+  const inputReais = document.getElementById('input-real').value
+  const realValueText = document.getElementById('real-value-text')
+  const dolarValueText = document.getElementById('dolar-value-text')
+
+
+  realValueText.innerHTML = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(inputReais)
+
+  if (select.value === "US$ Dólar") {
+    dolarValueText.innerHTML = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(inputReais / dolar)
+  }
+
+  if (select.value === "€ Euro") {
+    dolarValueText.innerHTML = new Intl.NumberFormat('de-DE', {
+      style: 'currency',
+      currency: 'EUR'
+    }).format(inputReais / euro)
+  }
+
+}
+
+changeCurrency = () => {
+  const currencyName = document.getElementById('currency-name')
+  const currencyImage = document.getElementById('currency-image')
+
+  if (select.value === 'US$ Dólar') {
+    currencyName.innerHTML = "Dólar Americano"
+    currencyImage.src = './img/estados-unidos.png'
+  }
+
+  if (select.value === '€ Euro') {
+    currencyName.innerHTML = "€ Euro"
+    currencyImage.src = './img/Euro.png'
+  }
+  convertValues ()
+}
+
+button.addEventListener('click', convertValues)
+select.addEventListener('change', changeCurrency)
