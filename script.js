396deebc42a7461fd0bfacd8bfe604cea831a416
Charles Byrne <125279820+Charles-Byrne@users.noreply.github.com>
2023-03-22 20:47:43 -0300

    Adicionando conversão bitcoin

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 const dolar = 5.24
 const euro = 5.7
+const bitcoin = 143.11004
 
 const convertValues = () => {
   const inputReais = document.getElementById('input-real').value
@@ -27,7 +28,9 @@ const convertValues = () => {
       currency: 'EUR'
     }).format(inputReais / euro)
   }
-
+  if (select.value === "Bitcoin") {
+    dolarValueText.innerHTML = (inputReais / bitcoin).toFixed(7)
+  }   
 }
 
 changeCurrency = () => {
@@ -43,7 +46,14 @@ changeCurrency = () => {
     currencyName.innerHTML = "€ Euro"
     currencyImage.src = './img/Euro.png'
   }
-  convertValues ()
+
+  if (select.value === 'Bitcoin') {
+    currencyName.innerHTML = "Bitcoin"
+    currencyImage.src = './img/Bit.png'
+  }
+
+  convertValues()
+
 }
 
 button.addEventListener('click', convertValues)
