9ea7cb92728b2912122198c394e2071c6a0c0fa5
Charles Byrne <125279820+Charles-Byrne@users.noreply.github.com>
2023-03-22 21:11:47 -0300

    atualização na variável bitcoin

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const button = document.getElementById('convert-button')
 const select = document.getElementById('currency-select')
 const dolar = 5.24
 const euro = 5.7
-const bitcoin = 143.11004
+const bitcoin = 143.110
 
 const convertValues = () => {
   const inputReais = document.getElementById('input-real').value
